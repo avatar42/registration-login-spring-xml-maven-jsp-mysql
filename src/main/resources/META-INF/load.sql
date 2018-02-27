9a6d4e7484c214176e1f2d37fe015a82474b08ef
avatar42 avatar42_github@m42.biz <avata@Hero>
2018-02-27 16:01:16 -0600

    Fix load to only load missing

--- src/main/resources/META-INF/load.sql
+++ src/main/resources/META-INF/load.sql
@@ -3,6 +3,8 @@
 --
 
 LOCK TABLES `role` WRITE;
-INSERT INTO `role` VALUES (1,'ROLE_USER');
+INSERT INTO `role` VALUES (1,'ROLE_USER') WHERE NOT EXISTS ( SELECT id from role where id=1) LIMIT 1;
+INSERT INTO `user`  (`username`, `password`) VALUES ('JimSmith','$2a$11$oH1vjQKYfPS2ddQWisiYj.z3qntBwLh6Ur2Ewy8brZbreEsx.gzXm') WHERE NOT EXISTS ( SELECT username from user where username='JimSmith') LIMIT 1;
+
 UNLOCK TABLES;
 
