5bebcc81229bc1c173c51ac3843a6b9ab1d65f2d
avatar42 avatar42_github@m42.biz <avata@Hero>
2018-02-28 11:17:16 -0600

    Load only if does not exist

--- src/main/resources/META-INF/load.sql
+++ src/main/resources/META-INF/load.sql
@@ -1,10 +1,12 @@
 --
--- Dumping data for table `role`
+-- passowrds = P@$$w0rd`
 --
 
 LOCK TABLES `role` WRITE;
-INSERT INTO `role` VALUES (1,'ROLE_USER') WHERE NOT EXISTS ( SELECT id from role where id=1) LIMIT 1;
-INSERT INTO `user`  (`username`, `password`) VALUES ('JimSmith','$2a$11$oH1vjQKYfPS2ddQWisiYj.z3qntBwLh6Ur2Ewy8brZbreEsx.gzXm') WHERE NOT EXISTS ( SELECT username from user where username='JimSmith') LIMIT 1;
+INSERT INTO `role`  SELECT * FROM (SELECT 1,'ROLE_USER') AS tmp WHERE NOT EXISTS ( SELECT id from role where id=1) LIMIT 1;
+
+INSERT INTO `user`  (`username`, `password`) SELECT * FROM (SELECT 'JimSmith','$2a$11$oH1vjQKYfPS2ddQWisiYj.z3qntBwLh6Ur2Ewy8brZbreEsx.gzXm') AS tmp  WHERE NOT EXISTS ( SELECT username from user where username='JimSmith') LIMIT 1;
+INSERT INTO `user`  (`username`, `password`) SELECT * FROM (SELECT 'Bobmith','$2a$11$oH1vjQKYfPS2ddQWisiYj.z3qntBwLh6Ur2Ewy8brZbreEsx.gzXm') AS tmp  WHERE NOT EXISTS ( SELECT username from user where username='BobSmith') LIMIT 1;
 
 UNLOCK TABLES;
 
