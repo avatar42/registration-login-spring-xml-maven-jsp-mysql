bfb19cd5b3ec4a72f9815a4cae87ecc4f8084c1b
avatar42 avatar42_github@m42.biz <avata@Hero>
2018-02-28 13:12:08 -0600

    add user_role loading

--- src/main/resources/META-INF/load.sql
+++ src/main/resources/META-INF/load.sql
@@ -7,7 +7,11 @@ INSERT INTO `role` VALUES (1,'ROLE_USER');
 UNLOCK TABLES;
 
 LOCK TABLES `user` WRITE;
-INSERT INTO `user`  (`username`, `password`) VALUES ( 'JimSmith','$2a$11$oH1vjQKYfPS2ddQWisiYj.z3qntBwLh6Ur2Ewy8brZbreEsx.gzXm');
-INSERT INTO `user`  (`username`, `password`) VALUES ('Bobmith','$2a$11$oH1vjQKYfPS2ddQWisiYj.z3qntBwLh6Ur2Ewy8brZbreEsx.gzXm');
+INSERT INTO `user`  VALUES ( 4,'JimSmith','$2a$11$oH1vjQKYfPS2ddQWisiYj.z3qntBwLh6Ur2Ewy8brZbreEsx.gzXm');
+INSERT INTO `user`  VALUES (5,'Bobmith','$2a$11$oH1vjQKYfPS2ddQWisiYj.z3qntBwLh6Ur2Ewy8brZbreEsx.gzXm');
 UNLOCK TABLES;
 
+LOCK TABLES `user_role` WRITE;
+INSERT INTO `user_role`  VALUES ( 4,1);
+INSERT INTO `user_role`  VALUES (5,1);
+UNLOCK TABLES;
